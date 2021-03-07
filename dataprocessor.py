e27094cc7994982978f4f971025ffbfc18fb50fb
Miska Sulander <mialsu@utu.fi>
2021-03-08 00:40:56 +0200

    minor update

--- dataprocessor.py
+++ dataprocessor.py
@@ -20,7 +20,7 @@ def correct_columns(data: DataFrame):
     data = data.dropna()
     # new columns to dataframe, values will be assigned later, description in README.md
     new_columns = ['HomePoints', 'AwayPoints', 'HGL5/90', 'AGL5/90', 'HGCL5/90', 'AGCL5/90', 'HSL5/90', 'ASL5/90', 'HSCL5/90', 'ASCL5/90', 'HSTL5/90', 'ASTL5/90', 'HSTCL5/90', 'ASTCL5/90' 
-                'HCL5/90', 'ACL5/90', 'HCCL5/90', 'ACCL5/90', 'HP%', 'AP%', 'HW%', 'AW%', 'HPL5%', 'APL5%', 'HWL5%', 'AWL5%', 'STot', 'SOTTot', 'CTot']               
+                'HCL5/90', 'ACL5/90', 'HCCL5/90', 'ACCL5/90', 'HP%', 'AP%', 'HW%', 'AW%', 'HPL5%', 'APL5%', 'HWL5%', 'AWL5%', 'GTot', 'STot', 'SOTTot', 'CTot']               
 
     empty = np.nan
     i = 12
@@ -58,11 +58,16 @@ with open('data/I1_15_16.csv', encoding='utf-8') as f:
 data = correct_columns(data)    # Manipulate raw data frame to contain all columns desired
 add_points(data)                # Add point values to point columns
 
-# Count totals in shots, shots on target and corners for each match
+# Count totals in goals, shots, shots on target and corners for each match
 for i in range(len(data)):
     data.at[i, 'STot'] = data.at[i, 'HS'] + data.at[i, 'AS']
     data.at[i, 'SOTTot'] = data.at[i, 'HST'] + data.at[i, 'AST']
     data.at[i, 'CTot'] = data.at[i, 'HC'] + data.at[i, 'AC']
+    data.at[i, 'GTot'] = data.at[i, 'FTHG'] + data.at[i, 'FTAG']
+
+
+
+
 
         
 
